d5e18fe0d8eac71d1820e9d0d74f967d2ba1cfa5
iain <iain@soulflyer.co.uk>
2021-10-04 10:11:12 +0700

    returns directory, basename, filename and extension given a file

--- convert.clj
+++ convert.clj
@@ -1,30 +1,10 @@
 #!/usr/bin/env bb
 
 (require '[babashka.fs :as fs])
-;;(require '[clojure.java-time :as time])
-
-;; # NV_DIR="/Users/iain/Documents/org-mode/NotationalVelocity/"
-
-;; # zetteldeft_id () {
-;; #     stat -f "%SB" -t"%y%m%d%H%M" $1
-;; # }
-
-;; # file_name () {
-;; #     local original_file_name=$1
-;; #     local bare_file_name=`basename $original_file_name .txt`
-;; #     local zdid=$(stat -f "%SB" -t"%y%m%d%H%M" "$1")
-;; #     echo $zdid$bare_file_name
-;; # }
-;; # file_name $1
-;; # # ls $NV_DIR
-
-;; 2109111923
-
-(* 7 8)
 
 (def now (java.time.ZonedDateTime/now))
 (def local-timezone (java.time.ZoneId/of "Asia/Saigon"))
-(def pattern (java.time.format.DateTimeFormatter/ofPattern "yymmHHmm"))
+(def pattern (java.time.format.DateTimeFormatter/ofPattern "yyMMddHHmm"))
 
 
 (def create-time
@@ -43,4 +23,15 @@
       local-timezone)
     pattern))
 
-(println (creation-date "./test.txt"))
+(let [incoming-file (first *command-line-args*)
+      filename (fs/file-name incoming-file)
+      parent (.toString (fs/real-path (fs/parent incoming-file)))
+      split (fs/split-ext incoming-file)
+      ;;    ext (fs/extension incoming-file)
+      ext (second split)
+      basename (fs/file-name (first split))]
+
+  (println parent)
+  (println basename)
+  (println ext)
+  (println (creation-date incoming-file)))
